6fe988ff8cb14252fb7e693ddf245ec6c868cb0d
Your Name <you@example.com>
2026-08-21 22:13:54 +0530

    Add int and float

---
+++ Basics/variables.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+
+//this is a comment.
+printf("Variables are containers to store data\n");
+
+int num1; //creating of a variable and specifying its data type.
+num1 = 2; //assignment of value in that variable.
+printf("%d is an integer.\n",num1); //printing the variable.
+
+printf("Floats are decimal type of numbers.\n");
+float num2 = 26.66;
+printf("%.2f is a float.\n",num2);
+
+
+    return 0;
+}
