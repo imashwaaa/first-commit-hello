1a9dfb93783f9d0eb5a27bf21fd9ae64d43a9e96
imashwaaa <im.ashwaaa@gmail.com>
2026-08-21 21:20:50 +0530

    Organize basic C files

--- hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main() {
-    printf("Hello World!\n");
-    printf("This is my C programming repo.\n");
-    printf("In this repo, I will post all my code I wrote to learn C Programming.\n");
-    printf("See you in again, siuuuu\n");
-    return 0;
-}
