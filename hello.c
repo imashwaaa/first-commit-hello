2ebba462e4e7054522db9b99fb8fee5b04d60637
imashwaaa <im.ashwaaa@gmail.com>
2026-08-21 20:15:57 +0530

    Add print statement

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    printf("Hello World!\n");
+    printf("This is my C programming repo.\n");
+    printf("In this repo, I will post all my code I wrote to learn C Programming.\n");
+    printf("See you in again, siuuuu\n");
+    return 0;
+}
