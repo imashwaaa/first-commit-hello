3caa9424b501991b17052df9b3cbb74ffc3a5163
Your Name <you@example.com>
2026-08-22 21:30:27 +0530

    Add Arithmetic operators

---
+++ Basics/operators.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    //arithmetic operators
+    int a = 10, b = 5;
+    printf("Sum of %d and %d is %d\n",a,b,(a+b));
+    printf("Difference of %d and %d is %d\n",a,b,(a-b));
+    printf("Product of %d and %d is %d\n",a,b,(a*b));
+    printf("Division of %d and %d is %d\n",a,b,(a/b));
+
+    
+
+    return 0;
+}
