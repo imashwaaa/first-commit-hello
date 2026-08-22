168ac265a3289f39825ee5df63ae3a8724ba647b
Your Name <you@example.com>
2026-08-22 17:14:05 +0530

    use scanf()

---
+++ Basics/input-output.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int age;
+    printf("Enter your age: ");
+    scanf("%d",&age);
+    //asks for an input.
+
+    printf("You are %d years old.\n",age);
+    //prints the value of input.
+
+    return 0;
+}
