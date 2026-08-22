1b40a1616b6d8290be9cfab534c5de78c83aeb80
Your Name <you@example.com>
2026-08-22 17:20:20 +0530

    Add char input

--- Basics/input-output.c
+++ Basics/input-output.c
@@ -8,5 +8,11 @@ int main() {
     printf("You are %d years old.\n",age);
     //prints the value of input.
 
+    //taking a char input
+    char ch;
+    printf("Enter your fav letter: ");
+    scanf("%c,&ch"); //taking a char input
+    printf("You chose %c as a character\n",ch);//display the output
+    
     return 0;
 }
